834155d540033c534335f54a871e36d8262f5d62
MrJulsen <geisterdrache02@gmail.com>
2025-02-04 17:05:08 +0100

    Added cantilever recipes

--- common/src/main/java/de/mrjulsen/paw/registry/ModItems.java
+++ common/src/main/java/de/mrjulsen/paw/registry/ModItems.java
@@ -1,10 +1,12 @@
 package de.mrjulsen.paw.registry;
 
+import com.simibubi.create.content.processing.sequenced.SequencedAssemblyItem;
 import com.tterrag.registrate.util.entry.ItemEntry;
 
 import de.mrjulsen.paw.PantographsAndWires;
 import de.mrjulsen.paw.item.PantographItem;
 import de.mrjulsen.wires.item.WireBaseItem;
+import net.minecraft.world.item.CreativeModeTabs;
 
 public class ModItems {
     public static final ItemEntry<WireBaseItem> CATENARY_WIRE = PantographsAndWires.REGISTRATE.item("catenary_wire", p -> new WireBaseItem(p, ModWireRegistry.CATENARY_WIRE))
@@ -17,12 +19,20 @@ public class ModItems {
         .tab(ModCreativeModeTab.MAIN_TAB.getKey())
         .register();
 
-    public static final ItemEntry<PantographItem> MOD_ICON = PantographsAndWires.REGISTRATE.item("mod_icon", p -> PantographItem.create(ModBlocks.PANTOGRAPH.get(), p, true))
-        .register();
-
     public static final ItemEntry<PantographItem> PANTOGRAPH = PantographsAndWires.REGISTRATE.item("pantograph", p -> PantographItem.create(ModBlocks.PANTOGRAPH.get(), p, false))
         .properties(p -> p)
         .tab(ModCreativeModeTab.MAIN_TAB.getKey())
+        .register();
+
+
+        
+
+    public static final ItemEntry<PantographItem> MOD_ICON = PantographsAndWires.REGISTRATE.item("mod_icon", p -> PantographItem.create(ModBlocks.PANTOGRAPH.get(), p, true))
+        .removeTab(CreativeModeTabs.SEARCH)
+        .register();
+
+    public static final ItemEntry<SequencedAssemblyItem> CANTILEVER_GREEN_INCOMPLETE = PantographsAndWires.REGISTRATE.item("cantilever_incomplete", SequencedAssemblyItem::new)
+        .removeTab(CreativeModeTabs.SEARCH)
         .register();
 
     
